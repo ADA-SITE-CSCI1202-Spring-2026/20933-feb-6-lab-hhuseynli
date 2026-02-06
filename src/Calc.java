555a79d167aa0eff4ea17c5a0380016c9d9833ce
Huseyn Huseynli <ht.huseynli@gmail.com>
2026-02-06 13:36:26 +0400

    Write examples

---
+++ src/Calc.java
@@ -0,0 +1,15 @@
+package src;
+public class Calc {
+    public static void main(String[] args) {
+        // don't activate inline suggestions
+        if (args.length != 2) {
+            System.out.println("Please provide two numbers as arguments.");
+            return;
+        }
+
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
---
+++ src/Sum.java
@@ -0,0 +1,20 @@
+package src;
+import java.util.Scanner;
+
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int num1 = input.nextInt();
+
+        System.out.println("Enter second number: ");
+        int num2 = input.nextInt();
+
+        input.close();
+
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
